e50e2d0bbc56bf324fa1b1565c1ff6c27a89c9f6
Abdullah Altulea <11511782+DarkionAvey@users.noreply.github.com>
2025-03-20 23:30:31 +0100

    🚀

---
+++ main.cpp
@@ -0,0 +1,516 @@
+// ==WindhawkMod==
+// @id              taskbar-centered-condensed
+// @name            Tray area next to task area
+// @description     Moves the tray area next to the task area of the taskbar, for Windows 11. Heavily based on m417z code
+// @version         1.0.0
+// @author          DarkionAvey,m417z
+// @github          https://github.com/DarkionAvey/windhawk-taskbar-centered-condensed
+// @include         explorer.exe
+// @architecture    x86-64
+// @compilerOptions -ldwmapi -lole32 -loleaut32 -lruntimeobject -lshcore
+// ==/WindhawkMod==
+
+// Original code is published under The GNU General Public License v3.0.
+//
+// For bug reports and feeback:
+// https://github.com/DarkionAvey/windhawk-taskbar-centered-condensed
+
+#include <windhawk_utils.h>
+
+#include <functional>
+
+#include <dwmapi.h>
+
+#undef GetCurrentTime
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Xaml.Automation.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/base.h>
+
+using namespace winrt::Windows::UI::Xaml;
+
+struct {
+    bool startMenuOnTheLeft;
+    int startMenuWidth;
+} g_settings;
+
+std::atomic<bool> g_unloading;
+
+HWND g_startMenuWnd;
+int g_startMenuOriginalWidth;
+HWND g_searchMenuWnd;
+int g_searchMenuOriginalX;
+
+typedef enum MONITOR_DPI_TYPE {
+    MDT_EFFECTIVE_DPI = 0,
+    MDT_ANGULAR_DPI = 1,
+    MDT_RAW_DPI = 2,
+    MDT_DEFAULT = MDT_EFFECTIVE_DPI
+} MONITOR_DPI_TYPE;
+
+STDAPI GetDpiForMonitor(HMONITOR hmonitor,
+                        MONITOR_DPI_TYPE dpiType,
+                        UINT* dpiX,
+                        UINT* dpiY);
+
+HWND GetTaskbarWnd() {
+    HWND hTaskbarWnd = FindWindow(L"Shell_TrayWnd", nullptr);
+
+    DWORD processId = 0;
+    if (!hTaskbarWnd || !GetWindowThreadProcessId(hTaskbarWnd, &processId) ||
+        processId != GetCurrentProcessId()) {
+        return nullptr;
+    }
+
+    return hTaskbarWnd;
+}
+
+FrameworkElement EnumChildElements(
+    FrameworkElement element,
+    std::function<bool(FrameworkElement)> enumCallback) {
+    int childrenCount = Media::VisualTreeHelper::GetChildrenCount(element);
+
+    for (int i = 0; i < childrenCount; i++) {
+        auto child = Media::VisualTreeHelper::GetChild(element, i)
+                         .try_as<FrameworkElement>();
+        if (!child) {
+            Wh_Log(L"Failed to get child %d of %d", i + 1, childrenCount);
+            continue;
+        }
+
+        if (enumCallback(child)) {
+            return child;
+        }
+    }
+
+    return nullptr;
+}
+
+FrameworkElement FindChildByName(FrameworkElement element, PCWSTR name) {
+    return EnumChildElements(element, [name](FrameworkElement child) {
+        return child.Name() == name;
+    });
+}
+
+FrameworkElement FindChildByClassName(FrameworkElement element,
+                                      PCWSTR className) {
+    return EnumChildElements(element, [className](FrameworkElement child) {
+        return winrt::get_class_name(child) == className;
+    });
+}
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+#include <winrt/Windows.UI.Composition.h>
+void AnimateMargin(FrameworkElement element, float toLeft, float toTop, float toRight, float toBottom) {
+     Wh_Log(L"AnimateMargin called");
+      auto compositor = winrt::Windows::UI::Xaml::Hosting::ElementCompositionPreview::GetElementVisual(element).Compositor();
+    auto animation = compositor.CreateVector4KeyFrameAnimation();
+
+    animation.InsertKeyFrame(1.0f, { toLeft, toTop, toRight, toBottom });
+    animation.Duration(std::chrono::milliseconds(300));  // Adjust the duration as needed
+
+    auto propertySet = winrt::Windows::UI::Xaml::Hosting::ElementCompositionPreview::GetElementVisual(element).Properties();
+    propertySet.InsertVector4(L"Margin", { static_cast<float>(element.Margin().Left),
+     static_cast<float>(element.Margin().Top), static_cast<float>(element.Margin().Right), static_cast<float>(element.Margin().Bottom) });
+    
+    propertySet.StartAnimation(L"Margin", animation);
+
+}
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+
+
+
+bool ApplyStyle(XamlRoot xamlRoot) {
+    auto xamlRootContent = xamlRoot.Content().try_as<FrameworkElement>();
+
+// todo: move notification center
+    auto taskbarFrameRepeater = FindChildByName(FindChildByName(FindChildByClassName(xamlRootContent, L"Taskbar.TaskbarFrame"), L"RootGrid"), L"TaskbarFrameRepeater");
+    auto trayFrame = FindChildByClassName(xamlRootContent, L"SystemTray.SystemTrayFrame");
+    if (!taskbarFrameRepeater || !trayFrame) return false;
+
+    auto trayFrameWidth = trayFrame.ActualWidth(), taskbarFrameRepeaterWidth = taskbarFrameRepeater.ActualWidth();
+    double rootWidth = xamlRoot.Size().Width;
+  
+  
+//   taskbar animation
+ 
+    taskbarFrameRepeater.Margin({0, 0, g_unloading ? 0 : trayFrameWidth, 0});
+
+
+// tray animations
+    auto trayVisual = winrt::Windows::UI::Xaml::Hosting::ElementCompositionPreview::GetElementVisual(trayFrame);
+    auto originalOffset = trayVisual.Offset();
+    Wh_Log(L"Original Offset: %f, %f, %f", originalOffset.x, originalOffset.y, originalOffset.z);
+
+float newXOffset=(rootWidth + taskbarFrameRepeaterWidth - trayFrameWidth) / 2;
+boolean movingInwards=originalOffset.x>newXOffset;
+    auto taskbarVisual = winrt::Windows::UI::Xaml::Hosting::ElementCompositionPreview::GetElementVisual(taskbarFrameRepeater);
+    auto compositor = taskbarVisual.Compositor();
+    float targetOffsetXTray = (g_unloading ? 0.0f : static_cast<float>(newXOffset));
+    auto trayAnimation = compositor.CreateVector3KeyFrameAnimation();
+    trayAnimation.InsertKeyFrame(1.0f, winrt::Windows::Foundation::Numerics::float3{ targetOffsetXTray, 0.0f, 0.0f });
+    if(movingInwards){
+    trayAnimation.DelayTime(winrt::Windows::Foundation::TimeSpan(std::chrono::milliseconds(370)));
+    }
+    trayVisual.StartAnimation(L"Offset", trayAnimation);
+
+    return true;
+}
+
+
+
+void* CTaskBand_ITaskListWndSite_vftable;
+
+void* CSecondaryTaskBand_ITaskListWndSite_vftable;
+
+using CTaskBand_GetTaskbarHost_t = void*(WINAPI*)(void* pThis, void** result);
+CTaskBand_GetTaskbarHost_t CTaskBand_GetTaskbarHost_Original;
+
+using CSecondaryTaskBand_GetTaskbarHost_t = void*(WINAPI*)(void* pThis,
+                                                           void** result);
+CSecondaryTaskBand_GetTaskbarHost_t CSecondaryTaskBand_GetTaskbarHost_Original;
+
+using std__Ref_count_base__Decref_t = void(WINAPI*)(void* pThis);
+std__Ref_count_base__Decref_t std__Ref_count_base__Decref_Original;
+
+XamlRoot XamlRootFromTaskbarHostSharedPtr(void* taskbarHostSharedPtr[2]) {
+    if (!taskbarHostSharedPtr[0] && !taskbarHostSharedPtr[1]) {
+        return nullptr;
+    }
+
+    // Reference: TaskbarHost::FrameHeight
+    constexpr size_t kTaskbarElementIUnknownOffset = 0x40;
+
+    auto* taskbarElementIUnknown =
+        *(IUnknown**)((BYTE*)taskbarHostSharedPtr[0] +
+                      kTaskbarElementIUnknownOffset);
+
+    FrameworkElement taskbarElement = nullptr;
+    taskbarElementIUnknown->QueryInterface(winrt::guid_of<FrameworkElement>(),
+                                           winrt::put_abi(taskbarElement));
+
+    auto result = taskbarElement ? taskbarElement.XamlRoot() : nullptr;
+
+    std__Ref_count_base__Decref_Original(taskbarHostSharedPtr[1]);
+
+    return result;
+}
+
+XamlRoot GetTaskbarXamlRoot(HWND hTaskbarWnd) {
+    HWND hTaskSwWnd = (HWND)GetProp(hTaskbarWnd, L"TaskbandHWND");
+    if (!hTaskSwWnd) {
+        return nullptr;
+    }
+
+    void* taskBand = (void*)GetWindowLongPtr(hTaskSwWnd, 0);
+    void* taskBandForTaskListWndSite = taskBand;
+    for (int i = 0; *(void**)taskBandForTaskListWndSite !=
+                    CTaskBand_ITaskListWndSite_vftable;
+         i++) {
+        if (i == 20) {
+            return nullptr;
+        }
+
+        taskBandForTaskListWndSite = (void**)taskBandForTaskListWndSite + 1;
+    }
+
+    void* taskbarHostSharedPtr[2]{};
+    CTaskBand_GetTaskbarHost_Original(taskBandForTaskListWndSite,
+                                      taskbarHostSharedPtr);
+
+    return XamlRootFromTaskbarHostSharedPtr(taskbarHostSharedPtr);
+}
+
+XamlRoot GetSecondaryTaskbarXamlRoot(HWND hSecondaryTaskbarWnd) {
+    HWND hTaskSwWnd =
+        (HWND)FindWindowEx(hSecondaryTaskbarWnd, nullptr, L"WorkerW", nullptr);
+    if (!hTaskSwWnd) {
+        return nullptr;
+    }
+
+    void* taskBand = (void*)GetWindowLongPtr(hTaskSwWnd, 0);
+    void* taskBandForTaskListWndSite = taskBand;
+    for (int i = 0; *(void**)taskBandForTaskListWndSite !=
+                    CSecondaryTaskBand_ITaskListWndSite_vftable;
+         i++) {
+        if (i == 20) {
+            return nullptr;
+        }
+
+        taskBandForTaskListWndSite = (void**)taskBandForTaskListWndSite + 1;
+    }
+
+    void* taskbarHostSharedPtr[2]{};
+    CSecondaryTaskBand_GetTaskbarHost_Original(taskBandForTaskListWndSite,
+                                               taskbarHostSharedPtr);
+
+    return XamlRootFromTaskbarHostSharedPtr(taskbarHostSharedPtr);
+}
+
+using RunFromWindowThreadProc_t = void(WINAPI*)(void* parameter);
+
+bool RunFromWindowThread(HWND hWnd,
+                         RunFromWindowThreadProc_t proc,
+                         void* procParam) {
+    static const UINT runFromWindowThreadRegisteredMsg =
+        RegisterWindowMessage(L"Windhawk_RunFromWindowThread_" WH_MOD_ID);
+
+    struct RUN_FROM_WINDOW_THREAD_PARAM {
+        RunFromWindowThreadProc_t proc;
+        void* procParam;
+    };
+
+    DWORD dwThreadId = GetWindowThreadProcessId(hWnd, nullptr);
+    if (dwThreadId == 0) {
+        return false;
+    }
+
+    if (dwThreadId == GetCurrentThreadId()) {
+        proc(procParam);
+        return true;
+    }
+
+    HHOOK hook = SetWindowsHookEx(
+        WH_CALLWNDPROC,
+        [](int nCode, WPARAM wParam, LPARAM lParam) -> LRESULT {
+            if (nCode == HC_ACTION) {
+                const CWPSTRUCT* cwp = (const CWPSTRUCT*)lParam;
+                if (cwp->message == runFromWindowThreadRegisteredMsg) {
+                    RUN_FROM_WINDOW_THREAD_PARAM* param =
+                        (RUN_FROM_WINDOW_THREAD_PARAM*)cwp->lParam;
+                    param->proc(param->procParam);
+                }
+            }
+
+            return CallNextHookEx(nullptr, nCode, wParam, lParam);
+        },
+        nullptr, dwThreadId);
+    if (!hook) {
+        return false;
+    }
+
+    RUN_FROM_WINDOW_THREAD_PARAM param;
+    param.proc = proc;
+    param.procParam = procParam;
+    SendMessage(hWnd, runFromWindowThreadRegisteredMsg, 0, (LPARAM)&param);
+
+    UnhookWindowsHookEx(hook);
+
+    return true;
+}
+
+void ApplySettingsFromTaskbarThread() {
+    Wh_Log(L"Applying settings");
+
+    EnumThreadWindows(
+        GetCurrentThreadId(),
+        [](HWND hWnd, LPARAM lParam) -> BOOL {
+            WCHAR szClassName[32];
+            if (GetClassName(hWnd, szClassName, ARRAYSIZE(szClassName)) == 0) {
+                return TRUE;
+            }
+
+            XamlRoot xamlRoot = nullptr;
+            if (_wcsicmp(szClassName, L"Shell_TrayWnd") == 0) {
+                xamlRoot = GetTaskbarXamlRoot(hWnd);
+            } else if (_wcsicmp(szClassName, L"Shell_SecondaryTrayWnd") == 0) {
+                xamlRoot = GetSecondaryTaskbarXamlRoot(hWnd);
+            } else {
+                return TRUE;
+            }
+
+            if (!xamlRoot) {
+                Wh_Log(L"Getting XamlRoot failed");
+                return TRUE;
+            }
+
+            if (!ApplyStyle(xamlRoot)) {
+                Wh_Log(L"ApplyStyles failed");
+                return TRUE;
+            }
+
+            return TRUE;
+        },
+        0);
+}
+
+void ApplySettings(HWND hTaskbarWnd) {
+    RunFromWindowThread(
+        hTaskbarWnd, [](void* pParam) { ApplySettingsFromTaskbarThread(); }, 0);
+}
+
+using IUIElement_Arrange_t =
+    HRESULT(WINAPI*)(void* pThis, const winrt::Windows::Foundation::Rect* rect);
+IUIElement_Arrange_t IUIElement_Arrange_Original;
+
+HRESULT WINAPI
+IUIElement_Arrange_Hook(void* pThis,
+                        const winrt::Windows::Foundation::Rect* rect) {
+
+    auto original = [=] { return IUIElement_Arrange_Original(pThis, rect); };
+
+    if (g_unloading) {
+        return original();
+    }
+
+    FrameworkElement element = nullptr;
+    (*(IUnknown**)pThis)
+        ->QueryInterface(winrt::guid_of<FrameworkElement>(),
+                         winrt::put_abi(element));
+    if (!element) {
+        return original();
+    }
+
+    auto className = winrt::get_class_name(element);
+    if (className != L"Taskbar.ExperienceToggleButton") {
+        return original();
+    }
+
+    auto automationId =
+        Automation::AutomationProperties::GetAutomationId(element);
+    if (automationId != L"StartButton") {
+        return original();
+    }
+
+    auto taskbarFrameRepeater =
+        Media::VisualTreeHelper::GetParent(element).as<FrameworkElement>();
+
+    element.Dispatcher().TryRunAsync(
+        winrt::Windows::UI::Core::CoreDispatcherPriority::High, [element]() {
+            auto taskbarFrameRepeater =
+                Media::VisualTreeHelper::GetParent(element)
+                    .as<FrameworkElement>();
+            Wh_Log(L"element.Dispatcher().TryRunAsync");
+
+            HWND hTaskbarWnd = GetTaskbarWnd();
+            if (hTaskbarWnd) {
+                ApplySettings(hTaskbarWnd);
+            }
+        });
+
+    return original();
+}
+
+bool HookTaskbarDllSymbols() {
+    HMODULE module = LoadLibrary(L"taskbar.dll");
+    if (!module) {
+        Wh_Log(L"Failed to load taskbar.dll");
+        return false;
+    }
+
+    WindhawkUtils::SYMBOL_HOOK taskbarDllHooks[] = {
+        {
+            {LR"(const CTaskBand::`vftable'{for `ITaskListWndSite'})"},
+            &CTaskBand_ITaskListWndSite_vftable,
+        },
+        {
+            {LR"(const CSecondaryTaskBand::`vftable'{for `ITaskListWndSite'})"},
+            &CSecondaryTaskBand_ITaskListWndSite_vftable,
+        },
+        {
+            {LR"(public: virtual class std::shared_ptr<class TaskbarHost> __cdecl CTaskBand::GetTaskbarHost(void)const )"},
+            &CTaskBand_GetTaskbarHost_Original,
+        },
+        {
+            {LR"(public: virtual class std::shared_ptr<class TaskbarHost> __cdecl CSecondaryTaskBand::GetTaskbarHost(void)const )"},
+            &CSecondaryTaskBand_GetTaskbarHost_Original,
+        },
+        {
+            {LR"(public: void __cdecl std::_Ref_count_base::_Decref(void))"},
+            &std__Ref_count_base__Decref_Original,
+        },
+    };
+
+    return HookSymbols(module, taskbarDllHooks, ARRAYSIZE(taskbarDllHooks));
+}
+
+bool HookTaskbarViewDllSymbols() {
+    WCHAR dllPath[MAX_PATH];
+    if (!GetWindowsDirectory(dllPath, ARRAYSIZE(dllPath))) {
+        Wh_Log(L"GetWindowsDirectory failed");
+        return false;
+    }
+    wcscat_s(
+        dllPath, MAX_PATH,
+        LR"(\SystemApps\MicrosoftWindows.Client.Core_cw5n1h2txyewy\Taskbar.View.dll)");
+    HMODULE module =
+        LoadLibraryEx(dllPath, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (!module) {
+        Wh_Log(L"Taskbar view module couldn't be loaded");
+        return false;
+    }
+    // Taskbar.View.dll
+    WindhawkUtils::SYMBOL_HOOK symbolHooks[] = {{
+        {LR"(public: __cdecl winrt::impl::consume_Windows_UI_Xaml_IUIElement<struct winrt::Windows::UI::Xaml::IUIElement>::Arrange(struct winrt::Windows::Foundation::Rect const &)const )"},
+        &IUIElement_Arrange_Original,
+        IUIElement_Arrange_Hook,
+    }};
+    return HookSymbols(module, symbolHooks, ARRAYSIZE(symbolHooks));
+}
+
+void LoadSettings() {
+    g_settings.startMenuOnTheLeft = Wh_GetIntSetting(L"startMenuOnTheLeft");
+    g_settings.startMenuWidth = Wh_GetIntSetting(L"startMenuWidth");
+}
+
+BOOL Wh_ModInit() {
+    Wh_Log(L">");
+
+    LoadSettings();
+    // needed for the xml tree
+    if (!HookTaskbarDllSymbols()) {
+        Wh_Log(L"HookTaskbarDllSymbols failed");
+        return FALSE;
+    }
+    // needed for dynamic updates
+    if (!HookTaskbarViewDllSymbols()) {
+        Wh_Log(L"HookTaskbarViewDllSymbols failed");
+        return FALSE;
+    }
+
+    // not needed?
+    // HMODULE dwmapiModule = LoadLibrary(L"dwmapi.dll");
+    // if (dwmapiModule) {
+    //     FARPROC pDwmSetWindowAttribute =
+    //             GetProcAddress(dwmapiModule, "DwmSetWindowAttribute");
+    //     if (pDwmSetWindowAttribute) {
+    //         Wh_SetFunctionHook((void *) pDwmSetWindowAttribute,
+    //                            (void *) DwmSetWindowAttribute_Hook,
+    //                            (void **) &DwmSetWindowAttribute_Original);
+    //     }
+    // }
+
+    return TRUE;
+}
+
+void Wh_ModAfterInit() {
+    Wh_Log(L">");
+
+    HWND hTaskbarWnd = GetTaskbarWnd();
+    if (hTaskbarWnd) {
+        ApplySettings(hTaskbarWnd);
+    }
+}
+
+void Wh_ModBeforeUninit() {
+    Wh_Log(L">");
+
+    g_unloading = true;
+
+    HWND hTaskbarWnd = GetTaskbarWnd();
+    if (hTaskbarWnd) {
+        ApplySettings(hTaskbarWnd);
+    }
+}
+
+void Wh_ModUninit() {
+    Wh_Log(L">");
+}
+
+void Wh_ModSettingsChanged() {
+    Wh_Log(L">");
+    LoadSettings();
+}
